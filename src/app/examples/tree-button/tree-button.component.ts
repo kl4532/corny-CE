384996a8101c6ac8643348cfdd46264a7dc328ed
kl4532 <kornellukasiak@gmail.com>
2020-01-20 10:41:16 +0100

    change type of id in mock data to number

--- src/app/examples/tree-button/tree-button.component.ts
+++ src/app/examples/tree-button/tree-button.component.ts
@@ -14,45 +14,45 @@ export class TreeButtonExComponent implements OnInit {
 
   categories = [
     {
-      "id" : "1",
+      "id" : 1,
       "name" : "cat1-start",
       "subcategory" : [ {
-        "id" : "1.1",
+        "id" : 1.1,
         "name" : "sub1",
         "subcategory" : [ {
-          "id" : "1.1.1",
+          "id" : 1.11,
           "name" : "sub1.1",
           "subcategory" : [ {
-            "id" : "1.1.1.1",
+            "id" : 1.111,
             "name" : "sub1.1.1-last-child"
           } ]
         } ]
       } ]
     }, 
     {
-      "id" : "2",
+      "id" : 2,
       "name" : "cat2",
       "subcategory" : [ {
-        "id" : "2.1",
+        "id" : 2.1,
         "name" : "sub1"
       }, {
-        "id" : "2.2",
+        "id" : 2.2,
         "name" : "sub2"
       }, {
-        "id" : "2.3",
+        "id" : 2.3,
         "name" : "sub3"
       } ]
     }, {
-      "id" : "3",
+      "id" : 3,
       "name" : "cat3"
     }, {
-      "id" : "4",
+      "id" : 4,
       "name" : "cat4"
     }, {
-      "id" : "5",
+      "id" : 5,
       "name" : "cat5"
     }, {
-      "id" : "6",
+      "id" : 6,
       "name" : "cat6"
     } 
 ]
