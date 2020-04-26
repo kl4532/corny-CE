624dca8e1c4ffeeee4715c4ac1daf431c5036825
kl4532 <kornellukasiak@gmail.com>
2020-04-26 08:48:24 +0200

    remove hamburger component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, ViewChild, ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
 import { SearchbarExComponent } from './examples/searchbar/searchbar.component';
 import { HideLinkExComponent } from './examples/hide-link/hide-link.component';
-import { HamburgerMenuExComponent } from './examples/hamburger-menu/hamburger-menu.component';
 import { TreeButtonExComponent } from './examples/tree-button/tree-button.component';
 
 
@@ -11,7 +10,11 @@ import { TreeButtonExComponent } from './examples/tree-button/tree-button.compon
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  components = [SearchbarExComponent, HideLinkExComponent, HamburgerMenuExComponent, TreeButtonExComponent];
+  components = [
+    SearchbarExComponent, 
+    HideLinkExComponent, 
+    TreeButtonExComponent
+  ];
   @ViewChild('container', {read: ViewContainerRef, static: true}) container: ViewContainerRef;
 
   constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
