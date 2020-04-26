0c51076e4e2fda201cd4f0ea77f7bd5e4587435a
kl4532 <kornellukasiak@gmail.com>
2020-04-26 11:19:47 +0200

    btn example

---
+++ src/app/examples/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ex-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonExComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
