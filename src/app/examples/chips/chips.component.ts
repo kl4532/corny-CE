4e47ff8030b6e0eacf565556e9700040c843e2d6
kl4532 <kornellukasiak@gmail.com>
2020-11-27 19:30:38 +0100

    added chips component

---
+++ src/app/examples/chips/chips.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Chip } from 'corny-components/lib/models/chip';
+
+@Component({
+  selector: 'ex-chips',
+  templateUrl: './chips.component.html',
+  styleUrls: ['./chips.component.scss']
+})
+export class ChipsExComponent implements OnInit {
+
+  constructor() {}
+
+  options: Chip[] = []
+
+  ngOnInit() {
+    this.options = [
+      {
+        name: "Chip one",
+        selected: true
+      },
+      {
+        name: "Chip two",
+        selected: false
+      },
+      {
+        name: "Chip three",
+        selected: true
+      },
+      {
+        name: "Chip four",
+        selected: false
+      },
+      {
+        name: "Chip five",
+        selected: false
+      },
+    ];
+  }
+}
