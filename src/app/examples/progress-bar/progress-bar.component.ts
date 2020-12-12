416f21f575ccd3ebd19be0c8b7deec7e15dd1184
kl4532 <kornellukasiak@gmail.com>
2020-12-12 21:12:08 +0100

    fix progress bar

--- src/app/examples/progress-bar/progress-bar.component.ts
+++ src/app/examples/progress-bar/progress-bar.component.ts
@@ -9,11 +9,33 @@ export class ProgressBarExComponent implements OnInit {
 
   started: boolean;
   timesUp: boolean;
+  valueCountdown: number;
+  valueProgress: number;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.valueProgress = 0;
+    this.valueCountdown = 100;
+  }
 
+  startCountdown() {
+    const interval = setInterval(()=> {
+      if(this.valueCountdown<=0) {
+        clearInterval(interval);
+      }
+      this.valueCountdown = this.valueCountdown - 0.1;
+    }, 100);
+  }
+
+  startProgress() {
+    const interval = setInterval(()=> {
+      if(this.valueProgress>=100) {
+        clearInterval(interval);
+      }
+      this.valueProgress = this.valueProgress + 0.1;
+    }, 100);
+  }
   reset() {
     this.started = false;
     this.timesUp = true;
