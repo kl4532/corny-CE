a820339339eefbd2d49d64227248cbc82b5bab69
kl4532 <kornellukasiak@gmail.com>
2021-03-29 21:00:34 +0200

    add toolbar component

---
+++ src/app/examples/toolbar/toolbar.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit, Input} from '@angular/core';
+
+@Component({
+  selector: 'ex-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss']
+})
+export class ToolbarExComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
